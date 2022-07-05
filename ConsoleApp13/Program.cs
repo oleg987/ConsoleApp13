a6f0aa815d603508e5bcd7b846b37feccd17cf9a
oleg fruntov <fruntovoleg@gmail.com>
2022-07-05 15:41:25 +0300

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            double y = 257.9;
+
+            x = (int)y;
+
+            y = x;
+            // 
+            Console.WriteLine(y);
+        }
+    }
+}
